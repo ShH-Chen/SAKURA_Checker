3727537eb41866ddbb786376675e654c596e56e7
ShH-Chen <1020413991@qq.com>
2016-05-21 15:37:02 +0800

    增加移动文件时的异常处理

--- SAKURA_Checker/Filemove.cs
+++ SAKURA_Checker/Filemove.cs
@@ -29,20 +29,23 @@ namespace SAKURA
             string filename = readPath + "\\C2power traces" + idx.ToString("d5") + ".dat";
             string head = "\t\t\t\tidx:" + idx.ToString("d5") + "\n";
             byte[] head_byte = Encoding.UTF8.GetBytes(head);
+            try
+            {
+                FileStream fsread = new FileStream(filename, FileMode.Open, FileAccess.Read);
 
-            FileStream fsread = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                int length = (int)fsread.Length;
+                byte[] buf = new byte[length + head_byte.Length];
 
-            int length = (int)fsread.Length;
-            byte[] buf = new byte[length + head_byte.Length];
+                Array.Copy(head_byte, buf, head_byte.Length);
 
-            Array.Copy(head_byte, buf, head_byte.Length);
+                fsread.Read(buf, head_byte.Length, length);
+                fsread.Close();
 
-            fsread.Read(buf, head_byte.Length, length);
-            fsread.Close();
+                fswrite.Write(buf, 0, buf.Length);
 
-            fswrite.Write(buf, 0, buf.Length);
-
-            File.Delete(filename);
+                File.Delete(filename);
+            }
+            catch { return; }
         }
 
         public void close()
--- SAKURA_Checker/MainForm.cs
+++ SAKURA_Checker/MainForm.cs
@@ -379,7 +379,7 @@ namespace SAKURA_Checker
         private void button_about_Click(object sender, EventArgs e)
         {
             MessageBox.Show("版本：1.0.0 ReleaseTime: 2016/5/19 16:07\n"+
-                            "\t把所有文件移入一个文件中" );
+                            "\t把所有文件移入一个文件中,增加异常处理" );
         }
     }
 }
